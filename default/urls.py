845c9ac847c8d38d90b58a3008a7282e0ff785d6
william <williamchuang0227@gmail.com>
2021-06-10 11:04:35 +0800

    新增投票主題

--- default/urls.py
+++ default/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('poll/', views.PollList.as_view()), 
     path('poll/<int:pk>/', views.PollDetail.as_view()), 
     path('option/<int:oid>/', views.PollVote.as_view()),
+    path('poll/create/',views.PollCreate.as_view()),
+    
 ]
